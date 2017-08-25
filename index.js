d46aace1e5475d60cda6863f1b9c3b66d394a271
Tyler Jurecki <jurecki07@yahoo.com>
2017-08-25 09:27:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express();
+const bodyParser = require("body-parser");
+const robos = require("./data.js");
+const mustacheExpress = require("mustache-express");
+// set up rendering engine mustache-express
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+    res.render('employee', robos);
+
+});
+
+app.get("/profile/:id", (req, res) => {
+    //create variable 
+    //Look at href on emp. 
+    //Find Indentifier(individuals... app.find) for parsing through data 
+
+    let reqId = req.params.id;
+    let foundUser = robos.users.find(user => user.id == reqId);
+    res.render('profile', { robos: foundUser });
+
+
+});
+
+
+
+
+
+// get and use url name parameter to find the matching user in the users array. 
+// use that object to render the profile page.
+
+
+
+
+
+
+
+
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World!');
+// });
+
+app.listen(3000, function () {
+    console.log('Successfully started express application!');
+});
+
